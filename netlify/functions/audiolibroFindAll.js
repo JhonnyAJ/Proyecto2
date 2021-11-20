fe1f1cd69ac3904f0cec7d4f2ee32f739e6fcb87
JhonnyAJ <55931058+JhonnyAJ@users.noreply.github.com>
2021-11-20 14:14:28 -0600

    Add files via upload

--- netlify/functions/audiolibroFindAll.js
+++ netlify/functions/audiolibroFindAll.js
@@ -1,14 +1,14 @@
 "use strict"
-
+console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
 const clientPromise = require('./mongoDB');
 const headers = require('./headersCORS');
-
+console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
 exports.handler = async (event, context) => {
-
+  console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
   if (event.httpMethod == "OPTIONS") {
     return { statusCode: 200, headers, body: "OK" };
   }
-	
+	console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
   try {
     const client = await clientPromise;
     console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
