16545da067e2916b9a1680fa6e6bcc4b82446042
JhonnyAJ <55931058+JhonnyAJ@users.noreply.github.com>
2021-11-20 11:24:47 -0600

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+<div id="app" class="container">
+  <div class="row"  style="margin-top: 15px">
+    <router-link class="three columns button button-primary" 
+      to="/">Home</router-link>
+    <router-link class="three columns button button-primary" 
+      to="/audiolibros">Audiolibros</router-link>
+  </div>
+ <router-view />
+  <div class="row">
+    <button disabled="disabled" class="twelve columns button-primary">
+      Copyright (c) 2020 - UNA
+    </button>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
