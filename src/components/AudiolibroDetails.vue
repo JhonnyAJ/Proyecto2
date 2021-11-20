7131553d11225013fbba4a1d6e1107a2b4d19bae
JhonnyAJ <55931058+JhonnyAJ@users.noreply.github.com>
2021-11-20 13:11:05 -0600

    Add files via upload

---
+++ src/components/AudiolibroDetails.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="row">
+   <div class="eleven column" style="margin-top: 5%">
+    <h2>{{title}}</h2>
+     <form>
+     <div class="row">
+      <div class="six columns">
+       <label for="titleInput">Nombre del audiolibro</label>
+       <input class="u-full-width" type="text"
+         v-model="audiolibro.nombreAudio">
+      </div>
+      <div class="six columns">
+       <label for="titleInput">URL del audiolibro</label>
+       <input class="u-full-width" type="text"
+         v-model="audiolibro.audiolibro">
+      </div>
+     </div>
+     
+     <div class="row">
+      <router-link class="button button-primary" 
+        to="/home">Back</router-link>
+       <a v-if='edit' class="button button-primary" style="float: right"
+         v-on:click="updateAudiolibro(audiolibro._id)">Actualiza</a>
+       <a v-if='create' class="button button-primary" style="float: right"
+         v-on:click="createAudiolibro()">Crear</a>
+     </div>
+    </form>
+  </div>
+</div>
+</template>
+
+<script>
+import { useRoute } from 'vue-router'
+
+export default {
+  name: "Detalles del audiolibro",
+  props: ['create','edit','create'],
+  data() {
+    return {
+      title: "Datos del audiolibro",
+      book: {}
+    }
+  },
+  mounted() {
+    const route = useRoute()
+    if (route.params.id != null)
+      this.findAudiolibro(route.params.id);
+    else {
+      this.audiolbro = {
+        '_id': Math.floor(Math.random()*100000000),'nombreAudio':'','audio':''};
+    }
+  },
+  methods: {
+    findAudiolibro: function(id) {
+      fetch(this.url+'/.netlify/functions/audiolibroFind/'+id,
+      { headers: {'Accept': 'application/json'}})
+      .then((response) => response.json())
+      .then((items) => {
+       this.audiolibro = items[0];
+      })
+    },
+    updateAudiolibro: function(id) {
+      fetch(this.url+'/.netlify/functions/audiolibroUpdate/'+id,
+        { headers: {'Content-Type':'application/json'},
+          method: 'PUT',
+          body: JSON.stringify(this.audiolibro)})
+        .then((data) => {
+          this.$router.push('/audiolibro');
+        }
+      )
+    },
+    createAudiolibro: function() {
+      fetch(this.url+'/.netlify/functions/audiolibroInsert',
+        { headers: {'Content-Type':'application/json'},
+          method: 'POST',
+          body: JSON.stringify(this.audiolibro)})
+        .then((data) => {
+           this.$router.push('/audiolibro');
+        }
+      )
+    }
+  }
+};
+</script>
---
+++ src/components/AudiolibroIndex.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="row">
+   <div style="margin-top: 5%">
+     <h2>{{title}}</h2>
+     <table><thead>
+       <tr>
+         <th>Nombre del Audio</th>
+        <th>Link del audio</th>
+        <th>Autor</th>
+        <th class="text-center">Actions</th>
+	   </tr>
+       </thead><tbody>
+       <tr v-for='audiolibro in audiolibros'>
+       <td>{{audiolbro.nombreAudio}}</td>
+       <td>{{audiolbro.audio}}</td>
+       <td>{{audiolibro.autor}}</td>
+       <td>
+       <router-link class="button"
+         :to="'/audiolibro/show/'+audiolibro._id">Mostrar</router-link>
+       &nbsp;
+       <router-link class="button"
+         :to="'/audiolibro/edit/'+audiolibro._id">Editar</router-link>
+       &nbsp;
+       <a class="button"
+         v-on:click="deleteAudiolibro(audiolibro._id)">Eliminar</a>
+       </td>
+       </tr></tbody>
+     </table>
+     <router-link class="button button-primary" 
+       to="/audiolibro/create">Nuevo</router-link>
+   </div>
+  </div>
+</template>
+
+<script>
+	
+export default {
+  name: "Audiolibro Index",
+  data() {
+    return {
+      title: 'Lista de audiolibros',
+      audiolibros: []
+    };
+  },
+  mounted() {
+    this.allAudiolibros()
+  },
+  methods: {
+    allAudiolibros() {
+      fetch(this.url+'/.netlify/functions/audiolibroFindAll',
+        { headers: {'Accept': 'application/json'}})
+        .then((response) => response.json())
+        .then((items) => {
+          this.audiolibros = items;
+        })
+     },
+     deleteAuthor(id) {
+       fetch(this.url+'/.netlify/functions/audiolibroDelete/'+id,
+         { headers: {'Content-Type': 'application/json'},
+           method: 'DELETE'})
+          .then((items) => {
+            this.allAudiolibros();
+          }
+        )
+     }
+  }
+};
+</script>
