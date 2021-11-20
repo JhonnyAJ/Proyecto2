a07057d7de64839aa76de281394fcfc597331483
JhonnyAJ <55931058+JhonnyAJ@users.noreply.github.com>
2021-11-20 13:15:22 -0600

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -7,6 +7,6 @@ import './assets/css/skeleton.css';
 
 const app = createApp(App);
   
-app.config.globalProperties.url = 'https://proyecto-mongo.netlify.app';
+app.config.globalProperties.url = 'https://proyecto2distribuidos.netlify.app';
 
 app.use(router).mount("#app");
