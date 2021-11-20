b28d66ef7e8706b81e4ba17793bfd67d21fbf3be
JhonnyAJ <55931058+JhonnyAJ@users.noreply.github.com>
2021-11-20 11:41:48 -0600

    Add files via upload

---
+++ src/components/Home.vue
@@ -0,0 +1,10 @@
+<!-- Home.vue -->
+<template>
+  <div>
+    <h2 style="margin-top: 15px">Mis AudioLibros</h2>
+      <p>Ordena todos tus audiolibros aqui</p>
+    <ul>
+      <li><router-link to='/audiolibros'>Informacion de audiolibros</router-link></li>
+    </ul>
+  </div>
+</template>
